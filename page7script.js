492d638ae16e487e8903e16066d404082dd349d0
schmeelk <schmeels@newschool.edu>
2024-05-16 14:29:14 -0400

    image update
    image update

--- page7script.js
+++ page7script.js
@@ -4,3 +4,18 @@ function onButtonClick() {
 
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
+
+
+window.onload = choosePic;
+
+var myPix = new Array(
+		"bird.jpg",
+        "cat.jpg",
+        "fireworks.jpg"
+		);
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+	 var selected_image = imyPix[random_index];
+     document.getElementById("myPicture").src =  `./media/captcha-images/public-domain-www.publicdomainpictures.net/${selected_image}`;
+	 
