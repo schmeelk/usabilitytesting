0ad5091edf38090c636754a988774b423c14e4ad
schmeelk <schmeels@newschool.edu>
2024-05-20 12:20:53 -0400

    update

--- page7script.js
+++ page7script.js
@@ -24,7 +24,7 @@ button.addEventListener('click', onButtonClick);
 
 var selected_image = 'cat.jpg'; 
  
-window.onload = choosePic;
+window.addEventListener(load, choosePic);
 
 var solve_start = -1;
 var solve_end = -1; 
@@ -40,33 +40,35 @@ function choosePic() {
 	 selected_image = imyPix[random_index];
      document.getElementById("myPicture").src =  `./media/captcha-images/public-domain-www.publicdomainpictures.net/${selected_image}`;
 	 
+
+	 
+	 
+	 
+	const sounds = {
+	  alt: {
+		url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+	  },
+	  bas: {
+		url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+	  },
+	  sopraan: {
+		url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+	  },
+	  tenor: {
+		url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+	  },
+	  total: {
+		url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+	  }
+	};
+
+	const membershipEl = document.querySelector("#membership");
+	const playerEl = document.querySelector("#player");
+
+	playerEl.src = sounds[membershipEl.value].url;
+	membershipEl.addEventListener("change", (e) => {
+	  playerEl.src = sounds[e.target.value].url;
+	});
+
+
 };
-	 
-	 
-	 
-const sounds = {
-  alt: {
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-  },
-  bas: {
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-  },
-  sopraan: {
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-  },
-  tenor: {
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-  },
-  total: {
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-  }
-};
-
-const membershipEl = document.querySelector("#membership");
-const playerEl = document.querySelector("#player");
-
-playerEl.src = sounds[membershipEl.value].url;
-membershipEl.addEventListener("change", (e) => {
-  playerEl.src = sounds[e.target.value].url;
-});
-
