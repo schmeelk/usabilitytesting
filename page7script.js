8d9792e1a27044799b0eee49a7e89f9b5eb47108
schmeelk <schmeels@newschool.edu>
2024-05-21 21:02:04 -0400

    Update page7script.js

--- page7script.js
+++ page7script.js
@@ -6,18 +6,18 @@ import { } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.j
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
+//const firebaseConfig = {
   // ...
   // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://usabilitymetrics-default-rtdb.firebaseio.com",
-};
+  //databaseURL: "https://usabilitymetrics-default-rtdb.firebaseio.com",
+//};
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
 
 
 // Initialize Realtime Database and get a reference to the service
-const database = firebase.database();
+//const database = firebase.database();
 
 
 function onPlaySoundButtonClick() {
@@ -68,9 +68,9 @@ function onSubmitButtonClick() {
 function writeUserData(param1, param2, param3, param4) {
   const db = getDatabase();
   set(ref(db, 'info/' + param1), {
-    username: test1,
-    email: test2,
-    profile_picture : test3
+    testa: test1,
+    testb: test2,
+    testc : test3
   });
 }
 
