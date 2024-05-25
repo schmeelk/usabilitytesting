8059a9f4b151998d73ea4e30dabeb192a9625e17
schmeelk <schmeels@newschool.edu>
2024-05-25 12:14:38 -0400

    Update page7script.js

--- page7script.js
+++ page7script.js
@@ -151,7 +151,7 @@ function submitDetails2JSON(exportData){
       //}));
 	  console.log(JSON.stringify(exportData, null, 2));
 	  a.href = URL.createObjectURL(new Blob([JSON.stringify(submitDetails2JSON(), null, 2)], {
-        type: "text/json"
+        type: "application/json"
       }));
 	  //JSON.parse(JSON.stringify(
 	 //let file = new Blob([exportData], {type: 'text/plain'});
